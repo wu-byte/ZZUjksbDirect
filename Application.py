480ae4edf31b5622b64529723c3048d2646d1875
ClokMuch <tm1596113667@gmail.com>
2021-09-22 21:29:23 +0800

    Initial.2021-09-22.最终测试版本，实际测试后将关闭 debug 标记进行发布.

--- Application.py
+++ Application.py
@@ -1,14 +1,18 @@
 # jksbDirect
 #
 # By Clok Much
-
 import sys
 import json
 import smtplib
 from email.mime.text import MIMEText
-import ssl
 
 import requests
+
+# ###### 调试区，项目可稳定使用时，两者均应是 False
+# 调试开关 正常使用请设定 False ，设定为 True 后会输出更多调试信息，且不再将真实姓名替换为 喵喵喵
+debug_switch = True
+# 总是认为上报失败的标记 正常使用请设定为 False ，设定为 True 后会每次都发送失败邮件，即使是上报成功
+always_fail = True
 
 # 开始时接收传入的 Secrets
 user_id = sys.argv[1]
@@ -19,7 +23,6 @@ mail_target = sys.argv[5]
 city_code = sys.argv[6]
 location = sys.argv[7]
 real_name = sys.argv[8]
-
 
 # 读取开放表单数据
 with open("config.json", "rb") as file_obj:
@@ -52,27 +55,38 @@ try:
                      }
 
         # 接收回应数据
-        response = session.post("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login", data=post_data, headers=header, verify=False)
+        response = session.post("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login", data=post_data, headers=header,
+                                verify=False)
         response.encoding = "utf-8"
-        out_log_ln56 = response.text
+        out_log_ln61 = response.text
         # 获取返回数据中的 ptopid 和 sid ，灌入 public_data
-        response_content = response.content[response.content.rfind(b'ptopid'):response.content.rfind(b'"}}\r\n</script>')]
+        response_content = response.content[response.content.rfind(b'ptopid'):response.content.rfind(b'"}}\r'
+                                                                                                     b'\n</script>')]
         mixed_token = str(response_content)[2:-1]
         if "hidden" in mixed_token:
             tried_calc += 1
             continue
         else:
             break
-
-    token_ptopid = mixed_token[7:mixed_token.rfind('&sid=')]
-    token_sid = mixed_token[mixed_token.rfind('&sid=')+5:]
-
-    public_data['ptopid'] = token_ptopid
-    public_data['sid'] = token_sid
+    try:
+        token_ptopid = mixed_token[7:mixed_token.rfind('&sid=')]
+        token_sid = mixed_token[mixed_token.rfind('&sid=') + 5:]
+        public_data['ptopid'] = token_ptopid
+        public_data['sid'] = token_sid
+    except NameError:
+        print("NameError_ln70-ln74，可能是学号或密码错误，也可能是服务器故障. 不再继续运行代码，"
+              "返回错误1，github应该会发送 Action 失败的邮件提醒.")
+        exit(1)
 
     # 填报表格
-    header["Referer"] = 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb'
-    response = requests.post('https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb', headers=header, data=public_data, verify=False)
+    try:
+        header["Referer"] = 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb'
+    except NameError:
+        print("NameError_ln81-ln82，在上一节中没有定义 header ，可能是学号或密码错误，也可能是服务器故障. "
+              "不再继续运行代码，返回错误1，github应该会发送 Action 失败的邮件提醒.")
+        exit(1)
+    response = requests.post('https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb', headers=header, data=public_data,
+                             verify=False)
 
     # 处理返回数据
     response.encoding = "utf-8"
@@ -88,7 +102,7 @@ if "感谢你今日上报" in result:
     print("上报成功")
 elif "由于如下原因" in result:
     result_flag = False
-    print("注意：上报失败！！代码需要更新，返回提示有新增或不匹配项目.")
+    print("注意：上报失败！！代码需要更新，返回提示有新增或不匹配项目，或是今日已被审核，而不能再上报.")
 elif "重新登录" in result:
     result_flag = False
     print("注意：上报失败！！可能是用户名或密码错误，或服务器响应超时.")
@@ -96,25 +110,37 @@ else:
     result_flag = False
     print("注意：上报失败！！原因未知，请自行检查返回结果和邮件中的变量输出.")
 
-# 调试区域，首次修改始终认为打卡失败，当确认运行有效后，删除对 result_flag 的重新赋值即可.
-result_flag = False
+# 始终发送邮件与否分支
+if always_fail:
+    result_flag = False
+# 调试信息详略分支
+if debug_switch:
+    private_debug = user_id + '★' + user_pd + '★' + mail_id + '★' + mail_pd + '★' + mail_target + '★'
+    private_debug = private_debug + city_code + '★' + location + '★' + real_name
+    this_time_vars = {'result_flag': result_flag,
+                      'mixed_token': mixed_token,
+                      'public_data': public_data,
+                      'final_header_ln82, first header_in_ln46': header,
+                      'out_log_ln58': out_log_ln61,
+                      'ln56_tried_calc': tried_calc,
+                      'secrets': sys.argv,
+                      'secrets_loaded': private_debug,
+                      'post_data_ln52-56': post_data,
+                      'result_ln78': result
+                      }
+else:
+    result = result.replace(real_name, "喵喵喵")
+    this_time_vars = {'mixed_token': mixed_token,
+                      'public_data': public_data,
+                      'ln56_tried_calc': tried_calc,
+                      'result': result
+                      }
 
 # 失败时发送邮件
 if not result_flag:
     with open("mail_public_config.json", 'rb') as file_obj:
         public_mail_config = json.load(file_obj)
         file_obj.close()
-    # 整理局部变量，结果如果包含真实姓名，将之替换为 喵喵喵
-    this_time_vars = {'result_flag': result_flag,
-                      'mixed_token': mixed_token,
-                      'public_data': public_data,
-                      'final_header': header,
-                      'out_log_ln56': out_log_ln56,
-                      'ln56_tried_calc': tried_calc,
-                      # 'real_name': real_name,
-                      # 'ags_input': [sys.argv[0:]]
-                      }
-    result = result.replace(real_name, "喵喵喵")
     # 配置邮件内容
     message = MIMEText(str(this_time_vars) + result, 'plain', 'utf-8')
     message['Subject'] = public_mail_config['title']
@@ -133,3 +159,5 @@ if not result_flag:
         print('打卡失败提示信息已发送到邮箱，内容包含个人敏感信息，请勿泄露邮件内容.')
     except smtplib.SMTPException as error_details:
         print('error', error_details)
+        print('发送结果的邮箱设置可能异常，请检查邮箱和密码配置，以及发信SMTP服务器配置.')
+        raise smtplib.SMTPException
