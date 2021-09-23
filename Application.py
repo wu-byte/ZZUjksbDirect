ad477525240a51a65e272156f6ec193963cdda65
ClokMuch <tm1596113667@gmail.com>
2021-09-23 14:03:34 +0800

    Initial.2021-09-23.最终测试版本，实际测试后将关闭 debug 标记进行发布，增加重试次数，修正错误代码处理过程.

--- Application.py
+++ Application.py
@@ -29,178 +29,182 @@ with open("config.json", "rb") as file_obj:
     public_data = json.load(file_obj)
     file_obj.close()
 
-# 固定私有数据，并合并表格项目
-private_user_id = user_id
-private_user_password = user_pd
+# 创建所有变量方便维护
 public_data['myvs_13a'] = city_code[:2]
 public_data['myvs_13b'] = city_code
 public_data['myvs_13c'] = location
+step_1_calc = 0
+step_1_output = False
+step_1_state = False
+step_2_calc = 0
+step_2_output = False
+step_2_state = False
+result_flag = 0
+response = False
+mixed_token = False
+all_input = sys.argv
+
+
+# 创建发送邮件的方法
+def report_mail(full_info=debug_switch):
+    if full_info:
+        this_time_vars = {'result_flag': result_flag,
+                          'mixed_token': mixed_token,
+                          'public_data': public_data,
+                          'step_1_output': step_1_output,
+                          'step_1_calc': step_1_calc,
+                          'step_1_state': step_1_state,
+                          'step_2_output': step_2_output,
+                          'step_2_calc': step_2_calc,
+                          'step_2_state': step_2_state,
+                          'secrets_inputted': sys.argv,
+                          'step_1_post_data': post_data,
+                          'result': result
+                          }
+    else:
+        replaced_result = result.replace(real_name, "喵喵喵")
+        this_time_vars = {'result_flag': result_flag,
+                          'step_1_output': step_1_output,
+                          'step_1_calc': step_1_calc,
+                          'step_1_state': step_1_state,
+                          'step_2_output': step_2_output,
+                          'step_2_calc': step_2_calc,
+                          'step_2_state': step_2_state,
+                          'step_1_post_data': post_data,
+                          'result': replaced_result
+                          }
+    with open("mail_public_config.json", 'rb') as file_obj_inner:
+        public_mail_config = json.load(file_obj_inner)
+        file_obj_inner.close()
+    # 配置邮件内容
+    mail_message = MIMEText(str(this_time_vars), 'plain', 'utf-8')
+    mail_message['Subject'] = public_mail_config['title']
+    mail_message['From'] = mail_id
+    mail_message['To'] = mail_target
+    # 尝试发送邮件
+    try:
+        smtp_obj = smtplib.SMTP_SSL(public_mail_config['host'], public_mail_config['port'])
+        smtp_obj.login(mail_id, mail_pd)
+        smtp_obj.sendmail(mail_id, mail_target, mail_message.as_string())
+        smtp_obj.quit()
+        print('具体提示信息已发送到邮箱，内容包含个人敏感信息，请勿泄露邮件内容.')
+        exit(0)
+    except smtplib.SMTPException:
+        print('发送结果的邮箱设置可能异常，请检查邮箱和密码配置，以及发信SMTP服务器配置.')
+        raise smtplib.SMTPException
+
 
 # 准备请求数据
-try:
-    session = requests.session()
-    info = {}
-    header = {"Origin": "https://jksb.v.zzu.edu.cn",
-              "Referer": "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0",
-              "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                            "Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.56",
-              "Host": "jksb.v.zzu.edu.cn"
-              }
-    post_data = {"uid": private_user_id,
-                 "upw": private_user_password,
-                 "smbtn": "进入健康状况上报平台",
-                 "hh28": "722",
-                 }
+session = requests.session()
+info = {}
+header = {"Origin": "https://jksb.v.zzu.edu.cn",
+          "Referer": "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                        "Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.56",
+          "Host": "jksb.v.zzu.edu.cn"
+          }
+post_data = {"uid": user_id,
+             "upw": user_pd,
+             "smbtn": "进入健康状况上报平台",
+             "hh28": "722",
+             }
 
-    tried_calc_1 = 0
-    response = False
-    while tried_calc_1 < 4:
+# 第一步 获取 token
+while step_1_calc < 4:
+    try:
         # 接收回应数据
-        response = session.post("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login", data=post_data, headers=header,
-                                verify=False)
-        response.encoding = "utf-8"
-        out_log_ln61 = response.text
-        # 获取返回数据中的 ptopid 和 sid ，灌入 public_data
-        response_content = response.content[response.content.rfind(b'ptopid'):response.content.rfind(b'"}}\r'
-                                                                                                     b'\n</script>')]
-        mixed_token = str(response_content)[2:-1]
-        if "hidden" in mixed_token:
-            tried_calc_1 += 1
-            continue
-        elif not mixed_token:
-            tried_calc_1 += 1
+        response = session.post("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login", data=post_data, headers=header)
+        if type(response) == requests.models.Response:
+            response.encoding = "utf-8"
+            step_1_output = response.text
+            if "验证码" in step_1_output:
+                print("运行时返回需要验证码，将终止本次打卡，您需要在 Action 中合理配置运行时间.")
+                report_mail(debug_switch)
+            mixed_token = response.text[response.text.rfind('ptopid'):response.text.rfind('"}}\r''\n</script>')]
+            if "hidden" in mixed_token:
+                step_1_calc += 1
+                continue
+            elif not mixed_token:
+                step_1_calc += 1
+                continue
+            else:
+                token_ptopid = mixed_token[7:mixed_token.rfind('&sid=')]
+                token_sid = mixed_token[mixed_token.rfind('&sid=') + 5:]
+                step_1_state = True
+                public_data['ptopid'] = token_ptopid
+                public_data['sid'] = token_sid
+                break
+        else:
+            if step_1_calc < 4:
+                print("获取 token 中" + str(step_1_calc)
+                      + "次失败，没有response，可能学校服务器故障，或者学号或密码有误，请检查返回邮件信息.")
+                continue
+            else:
+                print("获取 token 中" + str(step_1_calc)
+                      + "次失败，没有response，可能学校服务器故障，或者学号或密码有误，次数达到预期，终止本次打卡，报告失败情况.")
+    except requests.exceptions.SSLError:
+        if step_1_calc < 4:
+            print("获取 token 中" + str(step_1_calc)
+                  + "次失败，服务器提示SSLError，可能与连接问题有关.")
             continue
         else:
-            break
-    try:
-        token_ptopid = mixed_token[7:mixed_token.rfind('&sid=')]
-        token_sid = mixed_token[mixed_token.rfind('&sid=') + 5:]
-        public_data['ptopid'] = token_ptopid
-        public_data['sid'] = token_sid
-    except NameError:
-        print("NameError_ln70-ln74，可能是学号或密码错误，也可能是服务器故障. 不再继续运行代码，"
-              "返回错误1，github应该会发送 Action 失败的邮件提醒.")
-        exit(1)
-    try:
-        code_1 = response.status_code
-    except NameError:
-        try:
-            code_1 = response
-        except NameError:
-            code_1 = "NotExist"
+            print("获取 token 中" + str(step_1_calc)
+                  + "次失败，服务器提示SSLError，次数达到预期，终止本次打卡，报告失败情况.")
+            report_mail(debug_switch)
 
-    # 填报表格
-    header["Referer"] = 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb'
-    tried_calc_2 = 0
-    response = False
-    while tried_calc_2 < 4:
-        response = requests.post('https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb', headers=header, data=public_data,
-                                 verify=False)
-        response.encoding = "utf-8"
-        try:
-            code_2 = response.status_code
-        except NameError:
-            try:
-                code_2 = response
-            except NameError:
-                code_2 = False
-        if not code_2:
-            tried_calc_2 += 1
+# 第二步 填报表格
+header["Referer"] = 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb'
+# response = False
+while step_2_calc < 4:
+    try:
+        response = requests.post('https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb', headers=header, data=public_data, verify=False)
+        if type(response) == requests.models.Response:
+            response.encoding = "utf-8"
+            step_2_output = response.text
+            if "感谢你今日上报" in step_2_output:
+                break
+            elif "无权" in step_2_output:
+                print("填报表格中" + str(step_2_calc)
+                      + "次失败，次数达到预期，终止本次打卡，报告失败情况.")
+                report_mail(debug_switch)
+        else:
+            if step_2_calc < 4:
+                step_2_calc += 1
+                print("填报表格中" + str(step_2_calc)
+                      + "次失败，没有response，可能学校服务器故障，或者学号或密码有误，请检查返回邮件信息.")
+                continue
+            else:
+                print("填报表格中" + str(step_2_calc)
+                      + "次失败，没有response，可能学校服务器故障，次数达到预期，终止本次打卡，报告失败情况.")
+                report_mail(debug_switch)
+    except requests.exceptions.SSLError:
+        if step_2_calc < 4:
+            print("填报表格中" + str(step_2_calc)
+                  + "次失败，服务器提示SSLError，可能与连接问题有关.")
             continue
         else:
-            break
-
-    # 处理返回数据
-    result = response.text
-except requests.exceptions.SSLError:
-    result = ''
-    result_flag = False
-    print("requests.exceptions.SSLError，可能与无法联系到jksb服务器或secrets配置有误有关，详细问题报告到邮箱.")
-
+            print("填报表格中" + str(step_2_calc)
+                  + "次失败，服务器提示SSLError，次数达到预期，终止本次打卡，报告失败情况.")
+            report_mail(debug_switch)
 
 # 分析上报结果
+result = step_2_output
 if "感谢你今日上报" in result:
     result_flag = True
     print("上报成功")
 elif "由于如下原因" in result:
     result_flag = False
     print("注意：上报失败！！代码需要更新，返回提示有新增或不匹配项目，或是今日已被审核，而不能再上报.")
+    report_mail(debug_switch)
 elif "重新登录" in result:
     result_flag = False
     print("注意：上报失败！！可能是用户名或密码错误，或服务器响应超时.")
+    report_mail(debug_switch)
 else:
     result_flag = False
     print("注意：上报失败！！原因未知，请自行检查返回结果和邮件中的变量输出.")
+    report_mail(debug_switch)
 
-# 始终发送邮件与否分支
+# 总是发送邮件设定为 True 时，发送邮件
 if always_fail:
-    result_flag = False
-# 调试信息详略分支
-try:
-    tmp = mixed_token
-except NameError:
-    mixed_token = "no_token"
-try:
-    tmp = out_log_ln61
-except NameError:
-    out_log_ln61 = "no_in"
-
-try:
-    tmp = post_data
-except NameError:
-    post_data = "no_in"
-
-if debug_switch:
-    private_debug = user_id + '★' + user_pd + '★' + mail_id + '★' + mail_pd + '★' + mail_target + '★'
-    private_debug = private_debug + city_code + '★' + location + '★' + real_name
-    this_time_vars = {'result_flag': result_flag,
-                      'mixed_token': mixed_token,
-                      'public_data': public_data,
-                      'final_header_ln82, first header_in_ln46': header,
-                      'out_log_ln61': out_log_ln61,
-                      'ln56_tried_calc': tried_calc_1,
-                      'code_1': code_1,
-                      "tried_2": tried_calc_2,
-                      'code_2': code_2,
-                      'secrets': sys.argv,
-                      'secrets_loaded': private_debug,
-                      'post_data_ln52-56': post_data,
-                      'result_ln78': result
-                      }
-else:
-    result = result.replace(real_name, "喵喵喵")
-    this_time_vars = {'mixed_token': mixed_token,
-                      'public_data': public_data,
-                      'out_log_ln61': out_log_ln61,
-                      'ln56_tried_calc': tried_calc_1,
-                      'code_1': code_1,
-                      "tried_2": tried_calc_2,
-                      'code_2': code_2,
-                      'result': result
-                      }
-
-# 失败时发送邮件
-if not result_flag:
-    with open("mail_public_config.json", 'rb') as file_obj:
-        public_mail_config = json.load(file_obj)
-        file_obj.close()
-    # 配置邮件内容
-    message = MIMEText(str(this_time_vars), 'plain', 'utf-8')
-    message['Subject'] = public_mail_config['title']
-    message['From'] = mail_id
-    message['To'] = mail_target
-
-    # 尝试发送邮件
-    try:
-        smtpObj = smtplib.SMTP_SSL(public_mail_config['host'], public_mail_config['port'])
-        smtpObj.login(mail_id, mail_pd)
-        # 发送
-        smtpObj.sendmail(
-            mail_id, mail_target, message.as_string())
-        # 退出
-        smtpObj.quit()
-        print('打卡失败提示信息已发送到邮箱，内容包含个人敏感信息，请勿泄露邮件内容.')
-    except smtplib.SMTPException as error_details:
-        print('error', error_details)
-        print('发送结果的邮箱设置可能异常，请检查邮箱和密码配置，以及发信SMTP服务器配置.')
-        raise smtplib.SMTPException
+    report_mail(debug_switch)
