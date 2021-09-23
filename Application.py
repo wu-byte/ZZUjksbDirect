ed0f11df6954bd10d2a6ea150cdf905bc9a4aaec
ClokMuch <tm1596113667@gmail.com>
2021-09-23 14:17:57 +0800

    Initial.2021-09-23.最终测试版本，实际测试后将关闭 debug 标记进行发布，增加重试次数，修正错误代码处理过程.

--- Application.py
+++ Application.py
@@ -136,14 +136,17 @@ while step_1_calc < 4:
                 break
         else:
             if step_1_calc < 4:
+                step_1_calc += 1
                 print("获取 token 中" + str(step_1_calc)
                       + "次失败，没有response，可能学校服务器故障，或者学号或密码有误，请检查返回邮件信息.")
                 continue
             else:
                 print("获取 token 中" + str(step_1_calc)
                       + "次失败，没有response，可能学校服务器故障，或者学号或密码有误，次数达到预期，终止本次打卡，报告失败情况.")
+                report_mail(debug_switch)
     except requests.exceptions.SSLError:
         if step_1_calc < 4:
+            step_1_calc += 1
             print("获取 token 中" + str(step_1_calc)
                   + "次失败，服务器提示SSLError，可能与连接问题有关.")
             continue
@@ -179,6 +182,7 @@ while step_2_calc < 4:
                 report_mail(debug_switch)
     except requests.exceptions.SSLError:
         if step_2_calc < 4:
+            step_2_calc += 1
             print("填报表格中" + str(step_2_calc)
                   + "次失败，服务器提示SSLError，可能与连接问题有关.")
             continue
