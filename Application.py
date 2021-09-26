350954562a66c897b8497937f982d95be0ed9226
Clok Much <tm1596113667@gmail.com>
2021-09-27 07:15:19 +0800

    Update Application.py

--- Application.py
+++ Application.py
@@ -71,6 +71,8 @@ def report_mail(full_info=debug_switch):
     else:
         if type(result) == str:
             replaced_result = result.replace(real_name, "喵喵喵")
+        else:
+            replaced_result = result
         this_time_vars = {'result_flag': result_flag,
                           'step_1_output': step_1_output,
                           'step_1_calc': step_1_calc,
